5faa012bb1496877b636c593cd2d8f40be85bc79
CSLumbar <71946592+CSLumbar@users.noreply.github.com>
2022-02-17 18:07:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var password = "12345";
 
 function passcheck() {
   
-  if(document.getElementById('pass1').value ! = password) {
+  if(document.getElementById('pass1').value != password) {
     alert('Wrong Password, Try Again.');
     return false;
   }
