787fb8da3433ce7cf198f5f508118e8af57cbf56
CSLumbar <71946592+CSLumbar@users.noreply.github.com>
2022-02-17 14:21:35 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var password = "12345";
+
+function passcheck() {
+  
+  if(document.getElementById('pass1').value ! = password) {
+    alert('Wrong Password, Try Again.');
+    return false;
+  }
+  
+  if(document.getElementById('pass1').value == password) {
+    alert('Correct');
+    
+  }
+}
